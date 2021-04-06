Scaling down the code for correct FFT implementation

fwd_PV/fwd_lkl.py
@@ -8,13 +8,14 @@ import astropy.units as u
 from jax.config import config
 config.update("jax_enable_x64", True)
 
-from fwd_PV.velocity_box import ForwardModelledVelocityBox
+from fwd_PV.velocity_box import ForwardModelledVelocityBox, J
 
 EPS = 1e-50
+sig_v = 230.
 
 class ForwardLikelihoodBox(ForwardModelledVelocityBox):
-    def __init__(self, N_SIDE, L_BOX, PV_data, MB_data, smoothing_scale, window, Pk_type, Pk_data, N_POINTS=501):
-        super().__init__(N_SIDE, L_BOX, smoothing_scale, window, Pk_type, Pk_data)
+    def __init__(self, N_SIDE, L_BOX, PV_data, MB_data, N_POINTS=501):
+        super().__init__(N_SIDE, L_BOX)
         r_hMpc, e_rhMpc, RA, DEC, z_obs = PV_data
         r_hat = np.array(SkyCoord(ra=RA * u.deg, dec=DEC * u.deg).cartesian.xyz)
         self.r_hat = r_hat
@@ -59,12 +60,3 @@ class ForwardLikelihoodBox(ForwardModelledVelocityBox):
     def grad_lkl(self, delta_k, OmegaM, sig_v):
         lkl_grad = grad(self.log_lkl, 0)(delta_k, OmegaM, sig_v)
         return jnp.array([-lkl_grad[0], lkl_grad[1]])
-
-    def lnprob_Om(self, OmegaM, delta_k, sigma8, sig_v):
-        if((OmegaM < 0.1) or (OmegaM > 0.6)):
-            return -np.inf
-        return -self.log_lkl(delta_k, OmegaM, sig_v) + self.lnprob_s8(sigma8, OmegaM, delta_k)
-
-    def lnprob_sigv(self, sig_v, delta_k, OmegaM, sigma8):
-        logP = -self.log_lkl(delta_k, OmegaM, sig_v)
-        return logP

fwd_PV/velocity_box.py
@@ -7,37 +7,24 @@ from scipy.interpolate import interp1d
 from .tools.fft import grid_r_hat, Fourier_ks
 from .tools.cosmo import camb_PS, get_Pk
 
+J = jnp.array(np.complex(0,1))
+sig_v = 230.
+
 class ForwardModelledVelocityBox:
-    def __init__(self, N_GRID, L_BOX, smoothing_scale, window, Pk_type, Pk_data=None):
+    def __init__(self, N_GRID, L_BOX):
         self.L_BOX  = L_BOX
         self.N_GRID = N_GRID
         self.V = L_BOX**3
         l = L_BOX/N_GRID
-        self.J = jnp.array(np.complex(0, 1))
         self.l = l
         self.dV = l**3
         self.kh, self.pk_camb = camb_PS()
         self.OmegaM_true = 0.315
-        #self.sigma8_true = 0.7791
         self.sigma8_true = 0.8154
         self.k, self.k_norm = Fourier_ks(N_GRID, l)
-        if(Pk_type=='simple'):
-            self.sigma8_fid = self.sigma8_true
-        elif(Pk_type=='camb_interpolate'):
-            self.s8_arr, self.Om_arr, self.Pk_arr, self.kh = Pk_data 
-        self.Pk_type = Pk_type
-        if(window=='Gaussian'):
-            kR = self.k_norm * smoothing_scale
-            self.window = np.exp(-0.5 * kR**2)
-            self.kmax = 0.1
-        elif(window=='kmax'):
-            window_kmax = np.ones(self.k_norm.shape)
-            window_kmax[self.k_norm > smoothing_scale] = 0.
-            self.window = window_kmax
-            self.window_imag = window_kmax
-            print("self.window_imag.shape: "+str(self.window_imag.shape))
-            self.window_imag[:,:,-1] = 0.
-            self.kmax = smoothing_scale
+        smoothing_scale = 4.
+        kR = self.k_norm * smoothing_scale
+        self.window = np.exp(-0.5 * kR**2)
         self.r_hat_grid = grid_r_hat(N_GRID)
         self.Pk_interp = interp1d(self.kh, self.pk_camb)
         self.Pk_3d = self.get_Pk_3d()
@@ -48,47 +35,32 @@ class ForwardModelledVelocityBox:
         Pk_3d[select_positive_k] = self.Pk_interp(self.k_norm[select_positive_k])
         return Pk_3d
 
-    def Pk_var(self, sigma8, OmegaM, get_pk=False):
-        if(self.Pk_type=='simple'):
-            pk = (sigma8 / self.sigma8_fid)**2 * self.pk_camb
-        elif(self.Pk_type=='camb_interpolate'):
-            pk = get_Pk(sigma8, OmegaM, self.s8_arr, self.Om_arr, self.Pk_arr)
-        Pk_3d = 1e-20 * np.ones(self.k_norm.shape)
-        select_positive_k = (self.k_norm > 1e-15)
-        interp_pk = interp1d(self.kh, pk)
-        Pk_3d[select_positive_k] = interp_pk(self.k_norm[select_positive_k])
-        if(get_pk):
-            return jnp.array(Pk_3d), pk
-        return jnp.array(Pk_3d)
-
     def generate_delta_k(self):
-        Pk_var_3d, pk = self.Pk_var(self.sigma8_true, self.OmegaM_true, get_pk=True) 
+        Pk_var_3d = self.get_Pk_3d() 
         delta_k_var = Pk_var_3d / self.V / 2.
 
         delta_k_real = np.random.normal(0., np.sqrt(delta_k_var))
         delta_k_imag = np.random.normal(0., np.sqrt(delta_k_var))
         
-        print("delta_k_var.shape: "+str(delta_k_var.shape))
         delta_k_real[0,0,0] = 0.
         delta_k_imag[0,0,0] = 0. 
-        delta_k_imag[:,:,-1] = 0.
         
         return jnp.array([delta_k_real, delta_k_imag])
 
     def get_delta_grid(self, delta_k):
-        delta_k_complex = delta_k[0] + self.J * delta_k[1]
+        delta_k_complex = delta_k[0] + J * delta_k[1]
         delta_x = self.V / self.dV * jnp.fft.irfftn(delta_k_complex)
         return delta_x
    
     @partial(jit, static_argnums=(0,)) 
     def Vr_grid(self, delta_k, OmegaM=0.315):
-        delta_k_complex = delta_k[0] + self.J * delta_k[1]
+        delta_k_complex = delta_k[0] + J * delta_k[1]
         
         f = OmegaM**0.55
 
-        v_kx = self.J * 100 * self.window * f * delta_k_complex * self.k[0] / self.k_norm / self.k_norm
-        v_ky = self.J * 100 * self.window * f * delta_k_complex * self.k[1] / self.k_norm / self.k_norm
-        v_kz = self.J * 100 * self.window * f * delta_k_complex * self.k[2] / self.k_norm / self.k_norm
+        v_kx = J * 100 * self.window * f * delta_k_complex * self.k[0] / self.k_norm / self.k_norm
+        v_ky = J * 100 * self.window * f * delta_k_complex * self.k[1] / self.k_norm / self.k_norm
+        v_kz = J * 100 * self.window * f * delta_k_complex * self.k[2] / self.k_norm / self.k_norm
 
         vx = (jnp.fft.irfftn(v_kx) * self.V / self.dV)
         vy = (jnp.fft.irfftn(v_ky) * self.V / self.dV)
@@ -98,26 +70,18 @@ class ForwardModelledVelocityBox:
 
         return jnp.sum(V * self.r_hat_grid, axis=0)
     
-    def log_prior(self, delta_k, sigma8, OmegaM):
-        delta_k_var = self.Pk_var(sigma8, OmegaM) / self.V / 2.
+    def log_prior(self, delta_k):
+        delta_k_var = self.Pk_var() / self.V / 2.
         ln_prior_real = jnp.sum(0.5 * self.window * (delta_k[0]**2) / delta_k_var) + 0.5 * jnp.sum(self.window * jnp.log(delta_k_var))
-        ln_prior_imag = jnp.sum(0.5 * self.window_imag * (delta_k[1]**2) / delta_k_var) + 0.5 * jnp.sum(self.window_imag * jnp.log(delta_k_var))
+        ln_prior_imag = jnp.sum(0.5 * self.window * (delta_k[1]**2) / delta_k_var) + 0.5 * jnp.sum(self.window * jnp.log(delta_k_var))
         ln_prior = ln_prior_real + ln_prior_imag
         return ln_prior
 
-    def grad_prior(self, delta_k, sigma8, OmegaM):
-        return grad(self.log_prior, 0)(delta_k, sigma8, OmegaM)
+    def grad_prior(self, delta_k):
+        return grad(self.log_prior, 0)(delta_k)
     
     def psi(self, delta_k, sigma8, OmegaM, sig_v):
-        return self.log_prior(delta_k, sigma8, OmegaM) + self.log_lkl(delta_k, OmegaM, sig_v)
+        return self.log_prior(delta_k) + self.log_lkl(delta_k, sig_v)
 
-    def grad_psi(self, delta_k, sigma8, OmegaM, sig_v):
-        return self.grad_prior(delta_k, sigma8, OmegaM) + self.grad_lkl(delta_k, OmegaM, sig_v)
-
-    def lnprob_s8(self, sigma8, OmegaM, delta_k):
-        #print("sigma8: %2.7f, Omega_m: %2.7f"%(sigma8, OmegaM))
-        if((sigma8 < 0.3)or(sigma8 > 1.2)):
-            return -np.inf
-        logP = -self.log_prior(delta_k, sigma8, OmegaM)
-        #print("logP: %2.3f"%(logP))
-        return logP
+    def grad_psi(self, delta_k, sig_v):
+        return self.grad_prior(delta_k) + self.grad_lkl(delta_k, sig_v)

sample.py
@@ -18,27 +18,13 @@ assert restart_flag == 'INIT' or restart_flag == 'RESUME', "The restart flag (1s
 
 configfile = sys.argv[2]
 
-N_GRID, L_BOX, likelihood, sample_cosmology, sample_sigv, window, smoothing_scale, Pk_type,\
+N_GRID, L_BOX, likelihood,\
         N_MCMC, dt, N_LEAPFROG,\
-        datafile, savedir, N_SAVE, N_RESTART,\
-        fix_density, true_density_path = process_config(configfile)
+        datafile, savedir, N_SAVE, N_RESTART = process_config(configfile)
 
 assert likelihood == 'chi-squared' or likelihood == 'fwd_lkl', "The likelihood must be chi-squared or forward-likelihood."
 
-if(Pk_type=='simple'):
-    Pk_data = None
-elif(Pk_type=='camb_interpolate'):
-    with h5.File(savedir+'/Pk_arr.h5', 'r') as f:
-        Om_arr = f['Om_arr'][:]
-        s8_arr = f['s8_arr'][:]
-        Pk_arr = f['Pk_arr'][:]
-        kh     = f['kh'][:] 
-    Pk_data = [s8_arr, Om_arr, Pk_arr, kh]
-
 r_hMpc, e_rhMpc, RA, DEC, z_obs = process_datafile(datafile, 'h5')
-
-fix_Om = True
-fix_s8 = False
 
 if(likelihood=='chi-squared'):
     print("Initializing Chi-Squared Velocity Box....")
@@ -48,101 +34,51 @@ elif(likelihood=='fwd_lkl'):
     PV_data = [r_hMpc, e_rhMpc, RA, DEC, z_obs]
     MB_data = config_fwd_lkl(configfile)
     VelocityBox = ForwardLikelihoodBox(N_GRID, L_BOX, PV_data, MB_data, smoothing_scale, window, Pk_type, Pk_data)
- 
+
+sigma8 = 0.8154
+OmegaM = 0.315
+
 if(restart_flag=='INIT'):
-    #density_scaling = 0.1
-    density_scaling = 1.
-    if(fix_density):
-        delta_k = get_true_density(true_density_path, L_BOX, N_GRID)
-    else:
-        delta_k = density_scaling * VelocityBox.generate_delta_k()
-    sigma8 = 0.7791
-    OmegaM = 0.315
-    sig_v = 230.
+    density_scaling = 0.1
+    delta_k = density_scaling * VelocityBox.generate_delta_k()
     N_START = 0
 
 elif(restart_flag=='RESUME'):
     print("Restarting run....")
     f_restart = h5.File(savedir+'/restart.h5', 'r')
     N_START = f_restart['N_STEP'].value
-    if(fix_density):
-        delta_k = get_true_density(true_density_path, L_BOX, N_GRID)
-        Vr0 = VelocityBox.Vr_grid(delta_k)
-        print("Mean-std of Vr0: %2.4f, %2.4f"%(np.mean(Vr0), np.std(Vr0)))
-    else:
-        delta_k = f_restart['delta_k'][:]
-    try:
-        sigma8 = f_restart['sigma8'].value
-        OmegaM = f_restart['OmegaM'].value
-        sig_v = f_restart['sig_v'].value
-    except:
-        sigma8 = 0.7791
-        OmegaM = 0.315
-        sig_v = 230.
+    delta_k = f_restart['delta_k'][:]
     f_restart.close()
 
 mass_matrix = np.array([2. * VelocityBox.V / VelocityBox.Pk_3d, 2. * VelocityBox.V / VelocityBox.Pk_3d])
 density_sampler = HMCSampler(delta_k.shape, VelocityBox.psi, VelocityBox.grad_psi, mass_matrix, verbose=True)
 
 accepted = 0
-
-if(sample_cosmology):
-    if not fix_s8:
-        s8_sampler = SliceSampler(1, VelocityBox.lnprob_s8, 0.1)
-    if not fix_Om:
-        Om_sampler = SliceSampler(1, VelocityBox.lnprob_Om, 0.1)
-if(sample_sigv):
-    sigv_sampler = SliceSampler(1, VelocityBox.lnprob_sigv, 10.)
 dt = dt
 
 for i in range(N_START, N_START + N_MCMC):
-    if not fix_density:
-        start_time=time.time()
-        delta_k, ln_prob, acc = density_sampler.sample_one_step(delta_k, dt, N_LEAPFROG, psi_kwargs={"sigma8":sigma8, "OmegaM": OmegaM, "sig_v":sig_v}, grad_psi_kwargs={"sigma8":sigma8, "OmegaM":OmegaM, "sig_v":sig_v})
-        print("ln_prob: %2.4f"%(ln_prob))
-        if(acc):
-            print("Accepted")
-            accepted += 1
-        end_time = time.time()
-        print("Time taken: %2.4f"%(end_time - start_time))
-        acceptance_rate = accepted / (i - N_START + 1)
-        print("Current acceptance rate: %2.3f"%(acceptance_rate))
-    if(sample_cosmology):
-        start_cosmo_time = time.time()
-        print("Sampling cosmology...")
-        if not fix_Om:
-            print("Sampling Omega_m....")
-            OmegaM = Om_sampler.sample_one_step(OmegaM, lnprob_kwargs={"delta_k":delta_k, "sigma8":sigma8, "sig_v":sig_v})
-        if not fix_s8:
-            print("Sampling sigma8....")
-            sigma8 = s8_sampler.sample_one_step(sigma8, lnprob_kwargs={"delta_k":delta_k, "OmegaM":OmegaM})
-        print("sigma8: %2.4f, OmegaM:%2.4f"%(sigma8, OmegaM))
-        end_cosmo_time = time.time()
-        print("Time taken for cosmology sampling: %2.3f"%(end_cosmo_time - start_cosmo_time))
-    if(sample_sigv):
-        print("Sample sig_v...")
-        sig_v = sigv_sampler.sample_one_step(sig_v, lnprob_kwargs={"delta_k":delta_k, "OmegaM":OmegaM, "sigma8":sigma8})
-        print("sig_v: %2.2f"%(sig_v))
+    start_time=time.time()
+    delta_k, ln_prob, acc = density_sampler.sample_one_step(delta_k, dt, N_LEAPFROG)
+    print("ln_prob: %2.4f"%(ln_prob))
+    if(acc):
+        print("Accepted")
+        accepted += 1
+    end_time = time.time()
+    print("Time taken: %2.4f"%(end_time - start_time))
+    acceptance_rate = accepted / (i - N_START + 1)
+    print("Current acceptance rate: %2.3f"%(acceptance_rate))
     if(i%N_SAVE==0):
         print('=============')
         print('Saving file...')
         print('=============')
         j = i//N_SAVE
         f = h5.File(savedir + '/mcmc_'+str(j)+'.h5', 'w')
-        if not fix_density:
-            f['delta_k'] = delta_k
-            f['ln_prob'] = ln_prob
-        f['sigma8'] = sigma8
-        f['OmegaM'] = OmegaM
-        f['sig_v'] = sig_v
+        f['delta_k'] = delta_k
+        f['ln_prob'] = ln_prob
         f.close()
     if(i%N_RESTART==0):
         print("Saving restart file...")
         f = h5.File(savedir+'/restart.h5', 'w')
-        if not fix_density:
-            f['delta_k'] = delta_k
+        f['delta_k'] = delta_k
         f['N_STEP'] = i
-        f['sigma8'] = sigma8
-        f['OmegaM'] = OmegaM
-        f['sig_v'] = sig_v
         f.close()